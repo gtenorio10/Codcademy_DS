4c75618f2f63d0de093564a6f667c1d692d33da7
Gabriella <gtenorio10@gmail.com>
2020-04-17 22:51:19 -0700

    Answering the first 2 questions of project

--- Unit2/sql-population-project/population_queries.sql
+++ Unit2/sql-population-project/population_queries.sql
@@ -1,6 +1,16 @@
--- This is the first query:
+/* Find unique year and count how many unique year
+https://www.w3schools.com/sql/sql_count_avg_sum.asp */
+SELECT COUNT(DISTINCT year)
+FROM population_years;
+--There are 11 Distinct year
 
-SELECT DISTINCT year from population_years;
+/* What is the largest population size for Gabon in this dataset? */
+SELECT *
+FROM population_years
+WHERE country = 'Gabon'
+ORDER BY population DESC
+LIMIT 1;
+-- The largest population size for Gabon is 1.54526 millions in 2010
 
--- Add your additional queries below:
+
 
