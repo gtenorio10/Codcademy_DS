3d851d3ddb79f29fed8d6a42e7f429f48753e424
Gabriella <gtenorio10@gmail.com>
2020-04-20 17:01:37 -0700

    Adding Unit3 and coding challenge script

---
+++ Unit3/Code_Challenge.sql
@@ -0,0 +1,39 @@
+/* Use COUNT() and a LIKE operator to determine the number of users that have an email ending in ‘.com’. */
+SELECT COUNT(*)
+FROM users
+WHERE email LIKE "%.com";
+
+/* What are the most popular first names on Codeflix? */
+SELECT first_name, COUNT(*) AS "Count"
+FROM users
+GROUP BY first_name
+ORDER BY 2 DESC;
+
+/* The UX Research team wants to see a distribution of watch durations. They want the result to contain:
+
+duration, which is the watch event duration, rounded to the closest minute
+count, the number of watch events falling into this “bucket” */
+SELECT ROUND(watch_duration_in_minutes) AS "duration", COUNT(*) AS "count"
+FROM watch_history
+GROUP BY duration
+ORDER BY duration;
+
+/* Find all the users that have successfully made a payment to Codeflix and find their total amount paid.
+
+Sort them by their total payments (from high to low). */
+SELECT user_id, SUM(amount) AS amount
+FROM payments
+WHERE status = "paid"
+GROUP BY user_id
+ORDER BY amount DESC;
+
+/* Generate a table of user ids and total watch duration for users who watched more than 400 minutes of content.                                    */
+SELECT user_id, SUM(watch_duration_in_minutes) AS duration_total
+FROM watch_history
+GROUP BY user_id
+HAVING duratio_total > 400;
+
+/* To the nearest minute, how many minutes of content were streamed on Codeflix? */
+SELECT ROUND(SUM(watch_duration_in_minutes), 0) AS 'Overall_duration'
+FROM watch_history;
+
