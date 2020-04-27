462b797a7f210c44b960ca86266f3f829de19775
Gabriella <gtenorio10@gmail.com>
2020-04-27 16:01:21 -0700

    Updating stock market sql script

--- Independent_Project/stock_market_analysis.sql
+++ Independent_Project/stock_market_analysis.sql
@@ -16,13 +16,44 @@ FROM stocks
 WHERE price BETWEEN 90 AND 200
 ORDER BY price DESC;
 
+/* Highest stock of the table */
 SELECT name, MAX(price) AS "Maxium Price"
 FROM stocks;
 -- O'Reilly Automotive at $386.45
 
+/* Lowest stock of the table */
 SELECT name, MIN(price) AS "Minimum Price"
 FROM stocks;
 -- Hasbro Inc $72.34
 
+/* Finding the max, min and average of the stocks */
+SELECT name, MAX(price) AS 'Max Price', MIN(price) AS 'Min Price', ROUND(AVG(price),2) AS 'Avg Price'
+FROM stocks
+GROUP BY 1
+ORDER BY 2 DESC;
 
+/* Based on hours for all stocks all stocks */
+SELECT strftime("%H", datetime) AS 'Time (hrs)',
+MAX(price) AS 'Max Price', MIN(price) AS 'Min Price', ROUND(AVG(price),2) AS 'Avg Price'
+FROM stocks
+Group BY 1;
+
+/* Based on date for all stocks */
+SELECT strftime("%d", datetime) AS 'Date 04/__/20',
+MAX(price) AS 'Max Price', MIN(price) AS 'Min Price', ROUND(AVG(price),2) AS 'Avg Price'
+FROM stocks
+Group BY 1;
+
+/* Looking at T-Mobile by date & time */
+SELECT strftime("%d", datetime) AS 'Date 04/__/20',
+MAX(price) AS 'Max Price', MIN(price) AS 'Min Price', ROUND(AVG(price),2) AS 'Avg Price'
+FROM stocks
+WHERE symbol = "TMUS"
+Group BY 1;
+
+SELECT strftime("%H", datetime) AS 'Time (hrs)',
+MAX(price) AS 'Max Price', MIN(price) AS 'Min Price', ROUND(AVG(price),2) AS 'Avg Price'
+FROM stocks
+WHERE symbol = "TMUS"
+Group BY 1;
 
